d95708a6f4c07b2ddf23ce5340079cd5dce8aad9
full_stack_qa_admin <full_stack_qa_admin@protonmail.com>
2022-11-20 03:29:15 -0800

    first commit

---
+++ Test.py
@@ -0,0 +1,12 @@
+#First Python Program
+
+print("Hello World")
+
+num_list=[0,1,2,3,4,5]
+
+for item in num_list:
+    print(item)
+
+emp_info = {"name" : "Udit Batra","age" : "35", "location" : "pune"}
+
+print(emp_info)
